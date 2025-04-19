e954845b1649b65647ab55dbeeafbdbfd6c2bbe3
abdelboulgha <118727738+abdelboulgha@users.noreply.github.com>
2025-04-19 14:43:05 +0100

    my first commit

---
+++ app/src/main/java/com/example/urban_issue_reporter_mobile/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.urban_issue_reporter_mobile;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button b1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        // Initialisation du bouton
+        b1 = findViewById(R.id.button);
+
+        // Gestion du clic sur le bouton
+        b1.setOnClickListener(view -> {
+            // Démarrage de l'activité DrawActivity
+            Intent intent = new Intent(MainActivity.this, DrawerActivity.class);
+            startActivity(intent);
+        });
+
+        // Gérer les insets système (bordures, status bar, etc.)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
